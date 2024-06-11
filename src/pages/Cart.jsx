709052b53158e007494e9b5e96127db7a3d80e40
thanakorn <thanakornrakrian@gmail.com>
2024-06-11 14:43:06 +0700

    set router

---
+++ src/pages/Cart.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import MocNav from '../components/MocNav'
+
+const Cart = () => {
+  return (
+    <div>
+        <MocNav/>
+        Cart</div>
+  )
+}
+
+export default Cart
