76216ddec7f4ac200a944f6b5685ee144eecf1cb
thanakorn <thanakornrakrian@gmail.com>
2024-06-12 11:38:58 +0700

    create html design page filter and payment change modol credit card

--- src/components/MocNav.jsx
+++ src/components/MocNav.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 
-
 const MocNav = () => {
   return (
-    <div className='fixed bottom-2 right-2 w-1/2 bg-slate-300 flex justify-around rounded-md '>
+    <div className='fixed bottom-2 right-2 w-1/2 bg-slate-300 flex justify-around rounded-md z-10'>
         <Link className='hover:bg-slate-200 p-2 w-full text-center transition-all duration-200' to="/">Home</Link>
         <Link className='hover:bg-slate-200 p-2 w-full text-center transition-all duration-200' to="/Filter">Filter</Link>
         <Link className='hover:bg-slate-200 p-2 w-full text-center transition-all duration-200' to="/Cart">Cart</Link>
