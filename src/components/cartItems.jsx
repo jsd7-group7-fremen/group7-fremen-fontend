69b9676ea42c3a6a3b40fffbfebbf60d6575c5f0
04_Apiwat (Tee/ตี๋) <apiwat.pienwisetkaew@gmail.com>
2024-06-24 12:11:47 +0700

    link router and update cart.jsx, Navbar, itemsCart 04_tee

--- src/components/cartItems.jsx
+++ src/components/cartItems.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { useState } from "react";
 import {Link} from "react-router-dom"
 
 export default function CartItems({ cart, onQuantityChange, onSizeChange, onDeleteItem }) {
