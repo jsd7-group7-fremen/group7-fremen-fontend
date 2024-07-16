367f6c735722567bc2f0083d92ace1d762b2f6e9
thanakorn <thanakornrakrian@gmail.com>
2024-07-16 12:36:28 +0700

    change BASE_URL

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
-import { BASE_URL } from "./constants";
+// import { BASE_URL } from "./constants";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL;
 const PROD_URL = import.meta.env.VITE_PROD_URL;
 
 const axiosInstance = axios.create({
